332a8b57fe1e6888ae99f8d84ba8d0c2f88e273d
CicadaNCR <84196399+CicadaNCR@users.noreply.github.com>
2024-02-29 16:08:06 -0600

    useSendCurrency hook

--- src/hooks/useSendCurrency.ts
+++ src/hooks/useSendCurrency.ts
@@ -1,1 +1,28 @@
 import { useAccount, useChainId } from "wagmi";
+import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+
+import { erc20ABI } from "wagmi";
+
+export function useSendCurrency(
+    amount: bigint | undefined,
+    chainId: number | undefined,
+    tokenAddress: `0x${string}` | undefined,
+    from: `0x${string}` | undefined,
+    to: `0x${string}` | undefined,
+) {
+    const enabled = !!from && !!chainId && !!tokenAddress && !!from && !!to
+
+    const { config, error, isError } = usePrepareContractWrite({
+        address: tokenAddress,
+        abi: erc20ABI,
+        functionName: 'transfer',
+        args: to && amount ? [to, amount] : undefined,
+        chainId: chainId,
+        account: from,
+        enabled: enabled
+    })
+    
+    const { data, isLoading, isSuccess, write } = useContractWrite(config)
+
+    return { error, isError, isLoading, isSuccess, data, write }
+}

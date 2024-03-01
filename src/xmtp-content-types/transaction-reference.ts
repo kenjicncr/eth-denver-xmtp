03e0e5059e9b9a56dbf9d3f7fccc379c43a41d0c
Kenji <96749852+kenjicncr@users.noreply.github.com>
2024-03-01 11:55:29 -0700

    chore: changed namespace

--- src/xmtp-content-types/transaction-reference.ts
+++ src/xmtp-content-types/transaction-reference.ts
@@ -1,26 +1,32 @@
 import { ContentTypeConfiguration } from "@xmtp/react-sdk";
-import { ContentTypeTransactionReference, TransactionReferenceCodec } from "@xmtp/content-type-transaction-reference"
+import {
+  ContentTypeTransactionReference,
+  TransactionReferenceCodec,
+} from "@xmtp/content-type-transaction-reference";
 
 export const transactionReferenceContentTypeConfig: ContentTypeConfiguration = {
-    codecs: [new TransactionReferenceCodec()],
-    contentTypes: [ContentTypeTransactionReference.toString()],
-    namespace: "EVMMOIO", // Replace with the actual namespace you are using
-    processors: {
-        [ContentTypeTransactionReference.toString()]: [processTransactionReferenceRequest],
-    },
-    validators: {
-        [ContentTypeTransactionReference.toString()]: isValidTransactionReferenceContent,
-    },
+  codecs: [new TransactionReferenceCodec()],
+  contentTypes: [ContentTypeTransactionReference.toString()],
+  namespace: "evmmio", // Replace with the actual namespace you are using
+  processors: {
+    [ContentTypeTransactionReference.toString()]: [
+      processTransactionReferenceRequest,
+    ],
+  },
+  validators: {
+    [ContentTypeTransactionReference.toString()]:
+      isValidTransactionReferenceContent,
+  },
 };
 
 function processTransactionReferenceRequest(content: any) {
-    // Define how to process the multiply numbers content
-    // Example: logging, storing, or manipulating the data
+  // Define how to process the multiply numbers content
+  // Example: logging, storing, or manipulating the data
 }
 
 function isValidTransactionReferenceContent(content: any) {
-    // Define validation logic for multiply numbers content
-    // Example: checking if the numbers are valid, not null, etc.
-    // TODO
-    return true;
+  // Define validation logic for multiply numbers content
+  // Example: checking if the numbers are valid, not null, etc.
+  // TODO
+  return true;
 }
